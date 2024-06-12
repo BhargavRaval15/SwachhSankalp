8f2e6c9a4af1bdf398ae9c3a0ed1cdb5ed7e9b4b
BhargavRaval <bhargavraval27473@gmail.com>
2024-06-12 17:06:52 +0530

    Remove Passport for Authentication

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const passport = require("passport");
 const bcrypt = require("bcrypt");
+
+const authenticateUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username: username });
+    if (!user) {
+      req.flash("error_msg", "Incorrect username or password");
+      return res.redirect("/users/login");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      req.flash("error_msg", "Incorrect username or password");
+      return res.redirect("/users/login");
+    }
+    req.session.user = user;
+    req.flash("success_msg", "Logged in successfully");
+    next(); // Call next middleware or route handler
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -13,7 +34,7 @@ router.post("/register", async (req, res) => {
   let errors = [];
 
   if (!username || !password || !password2) {
-    error.push({ msg: "Please fill in all fields" });
+    errors.push({ msg: "Please fill in all fields" });
   }
   if (password !== password2) {
     errors.push({ msg: "Passwords do not match" });
@@ -26,15 +47,21 @@ router.post("/register", async (req, res) => {
   if (errors.length > 0) {
     res.render("register", { errors, username, password, password2 });
   } else {
-    const user = await User.findOne({ username: username });
-    if (user) {
-      errors.push({ msg: "Username already exists" });
-      res.render("register", { errors, username, password, password2 });
-    } else {
-      const newUser = new User({ username, password });
-      await newUser.save();
-      req.flash("success_msg", "You are now registered and can log in");
-      res.redirect("/users/login");
+    try {
+      const user = await User.findOne({ username: username });
+      if (user) {
+        errors.push({ msg: "Username already exists" });
+        res.render("register", { errors, username, password, password2 });
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+        req.flash("success_msg", "You are now registered and can log in");
+        res.redirect("/users/login");
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     }
   }
 });
@@ -43,18 +70,20 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/users/register",
-    failureFlash: true,
-  })(req, res, next);
+router.post("/login", authenticateUser, (req, res) => {
+  res.redirect("/");
 });
 
 router.get("/logout", (req, res) => {
-  req.logout();
-  req.flash("success_msg", "You are logged out");
-  res.redirect("/users/login");
+  req.session.destroy((err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      req.flash("success_msg", "You are logged out");
+      res.redirect("/users/login");
+    }
+  });
 });
 
 module.exports = router;
