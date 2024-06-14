6131c06075c6efb66a15475ee5efbf0b69c89aa3
BhargavRaval <bhargavraval27473@gmail.com>
2024-06-14 14:41:00 +0530

    Enhanced User Interface for public view

--- routes/quote.js
+++ routes/quote.js
@@ -1,14 +1,9 @@
+// routes/about.js
 const express = require("express");
 const router = express.Router();
-const Quote = require("../models/quote");
 
-router.get("/", async (req, res) => {
-  const quotes = await Quote.find({});
-  res.render("quote/index", { quotes });
-});
-router.get("/:id", async (req, res) => {
-  const quote = await Quote.findById(req.params.id);
-  res.render("quote/show", { quote });
+router.get("/", (req, res) => {
+  res.render("quote");
 });
 
 module.exports = router;
